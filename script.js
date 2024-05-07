e1ba362f2e243c26a3e6c4526157b05808cb7fda
Parthipan G <amail2parthivijay@gmail.com>
2024-05-07 22:32:47 +0530

    day 6 task added

---
+++ script.js
@@ -0,0 +1,8 @@
+var request = new XMLHttpRequest();
+request.open("GET","https://restcountries.com/v3.1/all");
+request.send();
+request.onload = function(){
+    var res = JSON.parse(request.response);
+    //res[1].subregion = "South India";
+
+}
